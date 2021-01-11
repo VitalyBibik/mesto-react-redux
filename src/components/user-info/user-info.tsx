b62d66333409db10ffbc1ac0d684ea4d24d31308
Vitaly Bibik <wital2010@gmail.com>
2021-01-11 16:51:03 +0300

    add user-info

---
+++ src/components/user-info/user-info.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './App.module.scss';
+import cn from "classnames";
+
+
+const userInfo = () => {
+  return (
+    <div className={cn(styles['user-info'])}>
+
+    </div>
+  );
+}
+
+export default userInfo;
+
