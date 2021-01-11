c3ab902d2f4cd6d735a4dac5b2805e9b230b672b
Vitaly Bibik <wital2010@gmail.com>
2021-01-11 12:39:09 +0300

    Add place-list

---
+++ src/components/place-list/places-list.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './App.module.scss';
+import cn from "classnames";
+
+
+const PlacesList = () => {
+  return (
+    <div className={cn(styles['places-list'])}>
+
+    </div>
+  );
+}
+
+export default PlacesList;
+
