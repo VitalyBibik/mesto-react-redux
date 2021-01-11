e16a78fbfc3e48265e89eb8cc910d1d94a37c5c4
Vitaly Bibik <wital2010@gmail.com>
2021-01-11 16:33:46 +0300

    add spinner

---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './App.module.scss';
+import cn from "classnames";
+
+
+const Spinner = () => {
+  return (
+    <div className={cn(styles['header'])}>
+
+    </div>
+  );
+}
+
+export default Spinner;
+
