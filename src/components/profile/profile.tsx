8931a38685a20752ffb173b332b48c253eba686c
Vitaly Bibik <wital2010@gmail.com>
2021-01-11 16:04:51 +0300

    add profile

---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './App.module.scss';
+import cn from "classnames";
+
+
+const Profile = () => {
+  return (
+    <div className={cn(styles['profile'])}>
+
+    </div>
+  );
+}
+
+export default Profile;
+
