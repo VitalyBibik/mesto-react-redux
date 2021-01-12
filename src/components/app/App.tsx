ce91e072e4c5c58a5f0e4a9335d4f2d053e3f038
Vitaly Bibik <wital2010@gmail.com>
2021-01-12 12:34:47 +0300

    Add profile

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import styles from "./App.module.scss";
 import cn from "classnames";
 import Header from "../header/header";
+import Profile from "../profile/profile";
 
 const App = () => {
   return (
     <div className={cn(styles["root"])}>
       <Header />
+      <Profile />
     </div>
   );
 };
--- src/components/user-info/user-info.tsx
+++ src/components/user-info/user-info.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import styles from './App.module.scss';
+import React from "react";
+import styles from "./user-info.module.scss";
 import cn from "classnames";
-
 
 const userInfo = () => {
   return (
-    <div className={cn(styles['user-info'])}>
-
+    <div className={cn(styles["user-info"])}>
+      <div className={cn(styles["user-info__photo"])} />
+      <div className={cn(styles["user-info__data"])}>
+        <h1 className={cn(styles["user-info__name"])}>Jaques Causteau</h1>
+        <p className={cn(styles["user-info__job"])}>Sailor, Researcher</p>
+        <button className={cn(styles["user-info__edit-btn"])}>Edit</button>
+      </div>
+      <button className={cn(styles["button"], styles["user-info__button"])}>
+        +
+      </button>
     </div>
   );
-}
+};
 
 export default userInfo;
-
