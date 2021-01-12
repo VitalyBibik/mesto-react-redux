f566023f08b00937c5b54d27591aa802be4da539
Vitaly Bibik <wital2010@gmail.com>
2021-01-12 15:55:32 +0300

    Add styles

--- src/components/place-list/places-list.tsx
+++ src/components/place-list/places-list.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
-import styles from './App.module.scss';
+import React from "react";
+import styles from "./App.module.scss";
 import cn from "classnames";
-
 
 const PlacesList = () => {
   return (
-    <div className={cn(styles['places-list'])}>
-
-    </div>
+    <div className={cn(styles["places-list"], styles["root__section"])}></div>
   );
-}
+};
 
 export default PlacesList;
-
