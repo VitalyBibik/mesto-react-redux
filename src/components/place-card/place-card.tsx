a3cdd2191c97b64f7d37d9a5f6c86123761c6c5f
Vitaly Bibik <wital2010@gmail.com>
2021-01-15 11:44:31 +0300

    render card on redux store

--- src/components/place-card/place-card.tsx
+++ src/components/place-card/place-card.tsx
@@ -2,26 +2,32 @@ import React from "react";
 import styles from "./place-card.module.scss";
 import cn from "classnames";
 
-const PlaceCard = ( { cards } : any) => {
-
-  return (
-    <div className={cn(styles["place-card"])}>
-      {cards.card.map((el: any) => {
-        return (
-        <div>
-          <img src={el.link}> className={cn(styles["place-card__image"])}</img>
-          <button className={cn(styles["place-card__delete-icon"])} />
-        </div>
-        <div className={cn(styles["place-card__description"])}>
-          <h3 className={cn(styles["place-card__name"])}>{el.name}</h3>
-          <div className={cn(styles["counter"])}>
-            <button className={cn(styles["place-card__like-icon"])}></button>
-            <p className={cn(styles["place-card__number-like"])}>{el.likes.length}</p>
+const PlaceCard = ({ cards }: any) => {
+  const renderCards = () => {
+    return cards.card.map((el: any) => {
+      return (
+        <>
+          <div
+            className={cn(styles["place-card__image"])}
+            style={{
+              backgroundImage: "url(" + el.link + ")",
+            }}
+          >
+            <button className={cn(styles["place-card__delete-icon"])} />
           </div>
-        </div>
-      }))
-    </div>
-  );
-}
-
+          <div className={cn(styles["place-card__description"])}>
+            <h3 className={cn(styles["place-card__name"])}>{el.name}</h3>
+            <div className={cn(styles["counter"])}>
+              <button className={cn(styles["place-card__like-icon"])} />
+              <p className={cn(styles["place-card__number-like"])}>
+                {el.likes.length}
+              </p>
+            </div>
+          </div>
+        </>
+      );
+    });
+  };
+  return <div className={cn(styles["place-card"])}>{renderCards()}</div>;
+};
 export default PlaceCard;
